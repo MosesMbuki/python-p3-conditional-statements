c52fee7f111253671b2cc602952873527f433bf8
+ <moses.mutitu@student.moringaschool.com>
2025-05-16 12:55:10 +0300

    done

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -2,16 +2,56 @@
 
 def admin_login(username, password):
     # your code here
+    username = username.lower()
+    if username == "admin" and password == "12345":
+        return "Access granted"
+    elif username == "admin" or password == "12345":
+        return "Access denied"
+    else:
+        return "Access denied"
     pass
 
 def hows_the_weather(temperature):
     # your code here
+    if temperature <= 33:
+        return "It's brisk out there!"
+    elif temperature == 55:
+        return "It's a little chilly out there!"
+    elif temperature == 75:
+        return "It's perfect out there!"
+    elif temperature == 99:
+        return "It's too dang hot out there!"
+    else:
+        return "That's not a temperature!"
     pass
 
 def fizzbuzz(num):
     # your code here
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
     pass
 
 def calculator(operation, num1, num2):
     # your code here
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            print("Division by zero is not allowed")
+            return None
+    else:
+        print("Invalid operation!")
+        return None
     pass
